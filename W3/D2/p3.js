64b44f436c0e9c5b19dd9e7972443573cbce22d7
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-03 15:42:30 +0530

    Logical operator

---
+++ W3/D2/p3.js
@@ -0,0 +1,6 @@
+//Logical operator
+console.log("logical operators")
+let a = true , b = false
+console.log("a&&b=",a&&b)//logical and operation
+console.log("a||b=",a||b)//logical or operation
+console.log("!a=",!a)//logical not operation
