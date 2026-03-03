f25cf844c6dc22b64daf1b955ae6decf34193447
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-03 15:41:51 +0530

    Comparison operator

---
+++ W3/D2/p2.js
@@ -0,0 +1,11 @@
+//Comparison operator
+console.log("Comparison operator")
+let x=5,y="5"
+console.log("x==y",x==y)//loose equality
+console.log("x===y",x===y)//strict equality
+console.log("x!=y",x!=y)//loose inequality
+console.log("x!==y",x!==y)//strict inequality
+console.log("X>3:",x>3)
+console.log("X<3:",x<3)
+console.log("X>=3:",x>=3)
+console.log("X<=3:",x<=3)
