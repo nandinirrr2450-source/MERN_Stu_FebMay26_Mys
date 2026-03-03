17441debc215383ae90bc3819f4c1a519680a851
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-03 15:45:10 +0530

    Arrow function

---
+++ W3/D2/p7.js
@@ -0,0 +1,21 @@
+//Arrow function
+(args)=>{
+    //body of the function
+}
+
+//add two numbers
+const sum = (a,b)=>{
+    return a+b
+}
+console.log("3+5=",sum(3,5))// this is one way 
+let =sum(3,6)//this is another way
+console.log(let)//way 
+
+//single line return or implicit return
+const square=(x)=> x*x
+console.log("square of 44 =",square(44))
+
+//
+const sayhello=()=>console.log("hello from arrow function")
+sayhello()
+
