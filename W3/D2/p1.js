6c6ab68244b7f4fd4f09632c13c0e19b862c5ce6
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-03 15:40:53 +0530

    Arithmetic operator

---
+++ W3/D2/p1.js
@@ -0,0 +1,16 @@
+//Arithmetic operator
+console.log("Arithmetic operator");
+const a=4
+const b=5
+let c=0
+console.log("a+b",c=a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a%b",a%b);//modulus
+console.log("a**b",a**b);//exponential
+//console.log("a++",a++);//post increment
+//console.log("++a",++a);//pre increment
+//console.log("a--",a--);//pre decrement
+//console.log("--a",--a);//post decrement
+
