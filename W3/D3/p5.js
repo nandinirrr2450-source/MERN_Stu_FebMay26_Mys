a0f7359f3e0a354a96e9a36bb26cd5481b7ef636
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-04 14:26:23 +0530

    Basics of objects

--- W3/D3/p5.js
+++ W3/D3/p5.js
@@ -3,3 +3,22 @@ const person={name:'Nandini',age:22,place:'Mysore',isstudent:false};
 console.log("name",person.name);//dot notation
 console.log("person",person);
 console.log("age",person['age']);//bracket notation
+
+//to add a new property
+person.city="Mysore";
+console.log("person=",person);
+
+//to modify
+person.age=18
+console.log("person=",person);
+
+//delete
+delete person.isstudent;
+console.log("person=",person);
+
+//object constructor
+const car=new Object();
+car.make="Audi";
+car.model="A4";
+car.year=2026;
+console.log("Car=",car);
