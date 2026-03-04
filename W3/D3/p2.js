e1b0affbc4bc8f021b3bf64230a3985dec042c07
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-04 13:25:21 +0530

    While loop

---
+++ W3/D3/p2.js
@@ -0,0 +1,16 @@
+//while loop
+console.log("while loop")
+
+let count=0
+while(count<10){
+    console.log(`count = ${count}`)
+    count++
+}
+
+console.log(" do while loop")
+//do while
+let value=0
+do{
+    console.log(`value =${value}`)
+    value++
+}while(value<3);
