d3a4bd8ad4a26fb5ee946c771694218122b569cd
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-04 13:27:46 +0530

    Basics of objects

---
+++ W3/D3/p5.js
@@ -0,0 +1,5 @@
+//Basics of objects 
+const person={name:'Nandini',age:22,place:'Mysore',isstudent:false};
+console.log("name",person.name);//dot notation
+console.log("person",person);
+console.log("age",person['age']);//bracket notation
