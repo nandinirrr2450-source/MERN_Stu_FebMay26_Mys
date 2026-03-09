97bf2f5d8fe9cf44bfe341b5f033179d77d4a58d
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-09 21:44:18 +0530

    Mouseover event js file

---
+++ W3/D5/p3.js
@@ -0,0 +1,14 @@
+//Mouse events
+const hoverBox=document.getElementById("hoverBox");
+
+hoverBox.addEventListener("mouseover",function(){
+    hoverBox.textContent ="Mouse is over me";
+    hoverBox.style.backgroundColor="green";
+
+});
+
+hoverBox.addEventListener("mouseout",function(){
+    hoverBox.textContent ="Mouse is over me";
+    hoverBox.style.backgroundColor="";
+
+});
