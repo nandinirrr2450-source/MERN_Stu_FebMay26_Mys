c36e50aff5139e6e9bf4627a6c7305f342f78ba3
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-17 19:10:29 +0530

    Assignment 5: Wallet with ‘this’ keyword

---
+++ ASSIGNMENT/javascriptFunctionsAssignment5.js
@@ -0,0 +1,52 @@
+//Assignment 5: Wallet with ‘this’ keyword
+const wallet = {
+    owner: "Nandini",
+    balance: 0,
+    lastTransaction: null,
+
+    deposit: function(amount) {
+
+        
+        if (typeof amount !== "number" || amount <= 0) {
+            console.log("Invalid deposit amount");
+            return;
+        }
+
+        this.balance += amount;
+
+        this.lastTransaction = {
+            type: "DEPOSIT",
+            amount: amount,
+            balanceAfter: this.balance
+        };
+    },
+
+    withdraw: function(amount) {
+
+    
+        if (typeof amount !== "number" || amount <= 0) {
+            console.log("Invalid withdraw amount");
+            return;
+        }
+
+        if (amount > this.balance) {
+            console.log("Insufficient balance");
+            return;
+        }
+
+        this.balance -= amount;
+
+        this.lastTransaction = {
+            type: "WITHDRAW",
+            amount: amount,
+            balanceAfter: this.balance
+        };
+    }
+};
+
+wallet.deposit(500);
+wallet.withdraw(200);
+//wallet.deposit(-50);    // invalid
+//wallet.withdraw(1000);  // insufficient
+
+console.log(wallet);
