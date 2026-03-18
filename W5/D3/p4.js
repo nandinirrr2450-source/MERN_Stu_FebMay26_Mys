836b1280a299ec92e0e17828f83fbbbd86585202
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-18 11:25:08 +0530

    combining multiple promise-based steps with async/await

---
+++ W5/D3/p4.js
@@ -0,0 +1,32 @@
+//combining multiple promise-based steps with async/await
+
+function getOrderId(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(501)
+        },500)
+    })
+}
+
+function getOrderDetails(orderId){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve({
+                id:orderId,
+                product:"Laptop",
+                quantity:2
+            })
+        },700)
+    })
+}
+
+async function showOrderSummary() {
+    const orderId= await getOrderId();
+    console.log("Order id:",orderId);
+
+    const orderDetails= await getOrderDetails(orderId);
+    console.log("Product Details: ",orderDetails.product)
+    console.log("Quantity: ",orderDetails.quantity)
+    
+}
+showOrderSummary()
