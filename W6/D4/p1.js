7a5b56addd97578d30e7981392be47738ae09de8
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-26 10:14:02 +0530

    Understanding HTTP Module

---
+++ W6/D4/p1.js
@@ -0,0 +1,7 @@
+//Understanding HTTP Module
+
+//Importing built-in HTTP module
+const http=require("http")
+
+console.log("Type of http.createServer: ",typeof http.createServer)
+console.log("common HTTP methods: ",http.METHODS)
