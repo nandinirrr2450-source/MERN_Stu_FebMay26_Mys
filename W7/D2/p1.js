0f63b1da181910e6331718bc3a84cae0b8873d69
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-03-31 16:59:47 +0530

    Routing : Nested routes

--- W7/D2/p1.js
+++ W7/D2/p1.js
@@ -29,3 +29,33 @@ app.listen(4000,function(){
     console.log("Express server running at http://localhost:4000");
 });
 
+
+//  New Router /
+//products router (handles/api/products/...)
+const productsRouter=express.Router()
+
+// /api/products  =>create product
+productsRouter.post("/",(req,res)=>{
+    res.json({
+        route: "/api/products",
+        message: "Create products"
+    })
+})
+
+// /api/products/:id   =>delete product
+productsRouter.delete("/:id",(req,res)=>{
+    res.json({
+        route: `/api/products/${req.params.id}`,
+        message: "Delete product"
+    })
+})
+
+// mount products router under /api/products
+apiRouter.use("/products",productsRouter)
+
+//to create data 
+// curl -X POST http://localhost:4000/api/products -H "content-type:application/json" -d "{"name":"Nandini","age":"22"}"
+// or
+//curl -X POST http://localhost:4000/api/products -H "content-type:application/json" -d "{\"name\":\"Nandini\",\"role\":\"admin\"}"
+//to delete 
+// curl -X DELETE  http://localhost:4000/api/products/12
