afc99ea38a83e594d6aed5599715f587391ddb3e
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-01 11:38:04 +0530

    Custom error classes

---
+++ W7/D3/p4.js
@@ -0,0 +1,41 @@
+//Custom error classes
+const express=require("express")
+const app=express()
+
+//custom error class
+class AppError extends Error{
+    constructor(message,statusCode){
+        super(message);
+        this.statusCode=statusCode;
+    }
+}
+app.get("/product",function(req,res,next){
+    next(new AppError("Products not found"),404)
+})
+
+app.get("/search",function(req,res,next){
+    if(!req.query.q){
+        return next(new AppError("Search query is required"),400)
+    }
+    res.json({
+        success:true,
+        query:req.query.q
+    })
+})
+
+//centralized error handling middleware customized
+app.use(function(error,req,res,next){
+    res.status(error.statusCode || 500).json({
+        success:false,
+        message: error.message,
+        statusCode: error.statusCode || 500
+    })
+})
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
+
+//http://localhost:4000/product
+//http://localhost:4000/search
+//http://localhost:4000/search?q=iphone
