8324003f5f4ad7bfd8d48bd0e77b6dddba1f304d
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-03 10:05:59 +0530

    cookie is used to track session ID

---
+++ W7/D5/p1.js
@@ -0,0 +1,15 @@
+//cookie is used to track session ID
+
+//simulated server-side session store
+const sessionStore={
+    "abc123":{
+        userId:101,
+        username: "Nandini",
+        role: "student"
+    }
+}
+
+//simulated browser cookie value
+const browserCookieSessionId="abc123"
+const sessionData=sessionStore[browserCookieSessionId]
+console.log("Server side session data: ",sessionData)
