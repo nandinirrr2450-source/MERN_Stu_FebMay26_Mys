95dd05054e9390cfe6db93cc07aa87eede51a684
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-06 10:47:50 +0530

    This file creates and exports a custom

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V1/events.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1/events.js
@@ -1,1 +1,8 @@
 //This file creates and exports a custom
+
+const EventEmitter=require("events")
+
+//custom EventEmmiter object
+const bookingEmitter=new EventEmitter()
+
+module.exports=bookingEmitter
