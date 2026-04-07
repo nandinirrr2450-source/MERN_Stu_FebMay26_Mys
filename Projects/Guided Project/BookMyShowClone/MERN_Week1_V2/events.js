3879ec9d3f90da3ed5d07a3dcc98bd6ca0500fa0
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-07 09:37:08 +0530

    Version 2 of week 1 created

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V2/events.js
@@ -0,0 +1,9 @@
+//This file creates and exports a custom
+
+const EventEmitter=require("events")
+
+//custom EventEmmiter object
+const bookingEmitter=new EventEmitter()
+
+
+module.exports=bookingEmitter
