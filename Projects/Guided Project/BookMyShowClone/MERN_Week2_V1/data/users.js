a6dd9453dc2cbff9783529f652b44de2f462fa34
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-08 10:06:48 +0530

    User's details of the application

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,20 @@
+//User's details of the application
+const users=[
+    {
+        id:1,
+        name:"Thilak",
+        email:"u@e.com",
+        password:"Thilak123",
+        role:"user"
+    },
+    {
+        id:2,
+        name:"Nandini",
+        email:"a@e.com",
+        password:"nandini123",
+        role:"admin"
+    }
+    
+]
+
+module.exports=users
