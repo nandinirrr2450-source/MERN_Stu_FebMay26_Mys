1bc39517199e7d4e5b2a35a784fb82e939295b78
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-08 10:30:08 +0530

    Middleware for request logMiddleware for request log

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/loggerMiddleware.js
@@ -0,0 +1,7 @@
+//Middleware for request log
+function loggerMiddleware(req,res,next){
+    console.log(`${req.method} ${req.originalUrl}`)
+    next()
+}
+module.exports=loggerMiddleware;
+//Task: write log to a file along with timestamp with both req & res
