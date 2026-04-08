019f1dd05e0a7ece4253982d36e2cd1b09ef34b0
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-08 10:01:29 +0530

    This file stores the movie data used in the CLI App

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/movies.js
@@ -0,0 +1,45 @@
+// This file stores the movie data used in the CLI App
+
+const movies = [
+    {
+        id:1,
+        title:"Dhurandar2",
+        language:"Hindi",
+        genre:"Action Thriller",
+        city:"Mysore",
+        cinema:"Nexus Mall-Screen 1",
+        showtimes:[
+            {time: "10:00 AM",seatsAvailable: 250},
+            {time: "1:00 PM",seatsAvailable: 250},
+            {time: "6:00 PM",seatsAvailable: 250}
+        ]
+    },
+    {
+        id:2,
+        title:"LoveMocktail3",
+        language:"Kannada",
+        genre:"Romantic",
+        city:"Mysore",
+        cinema:"DRC Mall-Screen 2",
+        showtimes:[
+            {time: "10:00 AM",seatsAvailable: 100},
+            {time: "1:00 PM",seatsAvailable: 90},
+            {time: "6:00 PM",seatsAvailable: 210}
+        ]
+    },
+    {
+        id:3,
+        title:"Hayagreeva",
+        language:"Kannada",
+        genre:"Action",
+        city:"Davangere",
+        cinema:"Davangere Mall-Screen 4",
+        showtimes:[
+            {time: "11:00 AM",seatsAvailable: 150},
+            {time: "2:00 PM",seatsAvailable: 200},
+            {time: "7:00 PM",seatsAvailable: 150}
+        ]
+    }
+];
+//Export the movie data so that other files can use it. 
+module.exports = movies;
