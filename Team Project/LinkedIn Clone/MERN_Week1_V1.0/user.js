d6043591380f8a6c69cbe43a57b3eede685181c3
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-15 15:38:28 +0530

    Handles user profile creation,login, updation

--- Team Project/LinkedIn Clone/MERN_Week1_V1.0/user.js
+++ Team Project/LinkedIn Clone/MERN_Week1_V1.0/user.js
@@ -1,21 +1,61 @@
-//Handles user profile creation, updates
+//Handles user profile creation,login, updation
 let users = [];
 let currentUser = null;
 
-function createProfile(name, headline) {
-    return new Promise((resolve) => {
+function createProfile(name, role, username, password) {
+    return new Promise((resolve, reject) => {
+
+        //check if username already exists
+        const exists = users.find(u => u.username === username);
+        if (exists) {
+            return reject("Username already exists");
+        }
+
         const user = {
             id: Date.now().toString(),
             name,
-            headline,
+            role,
+            username,
+            password,
             skills: [],
             experience: [],
             education: [],
             connections: []
         };
+
         users.push(user);
+        resolve(user);
+    });
+}
+
+//login function
+function login(username, password) {
+    return new Promise((resolve, reject) => {
+        const user = users.find(
+            u => u.username === username && u.password === password
+        );
+
+        if (!user) {
+            return reject("Invalid login credentials");
+        }
+
         currentUser = user;
         resolve(user);
+    });
+}
+
+//update function according to edit profile
+function updateProfile(updates) {
+    return new Promise((resolve, reject) => {
+        if (!currentUser) {
+            return reject("Please login first");
+        }
+        if (updates.role) currentUser.role = updates.role;
+        if(updates.skill) currentUser.skill=updates.skill;
+        if(updates.education) currentUser.education=updates.education;
+        if(updates.experience) currentUser.experience=updates.experience
+
+        resolve(currentUser);
     });
 }
 
@@ -33,6 +73,8 @@ function setCurrentUser(user) {
 
 module.exports = {
     createProfile,
+    login,
+    updateProfile,
     getCurrentUser,
     getAllUsers,
     setCurrentUser
