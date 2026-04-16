1e701b3d64ebaae4fca7f38ab758edd38dceb015
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-16 10:32:03 +0530

    Timestamp and advanced queries

---
+++ W8/D4/p2.js
@@ -0,0 +1,48 @@
+//Timestamp and advanced queries
+
+const mongoose = require('mongoose')
+
+async function main() {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/dateDB")
+        console.log("Connected to MongoDB")
+
+        const demoschema = new mongoose.Schema({
+            name: String
+        },
+            {
+                timestamps: true
+            }
+        )
+
+        const Model = mongoose.model('LogTime', demoschema)
+        // await Model.deleteMany()
+
+        // const data=await Model.create([
+        //     { name: "anusha" },
+        //     { name: "abhishek" },
+        //     { name: "rahul" }
+        // ])
+        // console.log("data:",data)
+
+        // const recent=await Model.find({
+        //     createdAt:{
+        //         //
+        //         $gte: new Date(Date.now()-900000)
+        //     }
+        // })
+        //console.log("Recent Data:",recent)
+
+        const recentdata=await Model.find().sort({createdAt:-1})
+        console.log("Recent Data:",recentdata)
+
+    }
+    catch (error) {
+        console.error("Error: ", error.message)
+    }
+    finally {
+        await mongoose.disconnect()
+        console.log("connection disconnected")
+    }
+}
+main()
