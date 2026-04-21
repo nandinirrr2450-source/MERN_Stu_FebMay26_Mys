ea8e263faba3b865508ba5a397ef1644f61b5abd
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-21 15:07:06 +0530

    added auth routes

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,13 +1,18 @@
-const express = require("express")
-const app=express()
+const express = require("express");
+const authRoutes = require("./routes/auth.routes");
 
-app.use(express.json())
+const app = express();
 
+app.use(express.json());
+app.use("/api/auth",authRoutes);
+
+//Base URL
 app.get("/",(req,res)=>{
     res.status(200).json({
-    success: true,
-    message: "Movie booking API is running ...."
-    })
-})
+        success:true,
+        message: "Movie booking API is running...",
+    });
+});
 
-module.exports=app
+
+module.exports = app;
