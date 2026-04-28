6f7c3728eae6fa36f038b06c5d1997876c89d483
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-28 15:41:00 +0530

    added prop destructuring

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { PropBasics} from "./components/p1.jsx"
+import {PropDestructuring} from "./components/p2.jsx"
 import './App.css'
 
 function App() {
 
   return (
     <>
-        < PropBasics/>
+        {/* < PropBasics/> */}
+        <PropDestructuring/>
     </>
   )
 }
