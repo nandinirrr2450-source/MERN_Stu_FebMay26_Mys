7b517493175c7875c5d264b520cb7de8e2cda63a
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-28 15:24:26 +0530

    prop basics

---
+++ W9/D2/prop-example-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { PropBasics} from "./components/p1.jsx"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        < PropBasics/>
+    </>
+  )
+}
+
+export default App
