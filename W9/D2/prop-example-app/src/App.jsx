1c6fe6777d50884c4267b66855a073594468d92e
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-29 09:51:49 +0530

    container or wrapper component added

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { PropBasics} from "./components/p1.jsx"
 import {PropDestructuring} from "./components/p2.jsx"
+import {PropsChildren} from "./components/p3.jsx"
 import './App.css'
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
   return (
     <>
         {/* < PropBasics/> */}
-        <PropDestructuring/>
+        {/* <PropDestructuring/> */}
+        <PropsChildren/>
     </>
   )
 }
