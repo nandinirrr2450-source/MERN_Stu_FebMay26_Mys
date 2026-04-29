76221635bb72fdf710036909b9c138a334cc401b
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-29 11:43:31 +0530

    added state lifting

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -3,6 +3,7 @@ import { PropBasics} from "./components/p1.jsx"
 import {PropDestructuring} from "./components/p2.jsx"
 import {PropsChildren} from "./components/p3.jsx"
 import { PropDrillingDemo } from "./components/p4.jsx"
+import { SharedStateParent } from "./components/p5.jsx"
 import './App.css'
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
         {/* < PropBasics/> */}
         {/* <PropDestructuring/> */}
         {/* <PropsChildren/> */}
-        <PropDrillingDemo/>
+        {/* <PropDrillingDemo/> */}
+        <SharedStateParent/>
     </>
   )
 }
