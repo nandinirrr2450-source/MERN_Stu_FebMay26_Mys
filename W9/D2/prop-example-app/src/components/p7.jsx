eddae9c33e55b080884ca5f2e6d28d425c994c95
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-04-29 12:43:03 +0530

    Passing objects and arrays as props

---
+++ W9/D2/prop-example-app/src/components/p7.jsx
@@ -0,0 +1,34 @@
+//Passing objects and arrays as props
+function UserCard({user}){
+    return(
+        <div className="card">
+            <p>Name: {user.name}</p>
+            <p>Role: {user.role}</p>
+        </div>
+    )
+}
+
+function SkillsList({skills}){
+    return(
+        <ul>
+            {skills.map((skill)=>(
+                <li key={skill}>{skill}</li>
+            ))}
+        </ul>
+    )
+}
+
+ export function ObjectsArrayProps(){
+    const user={
+        name: 'Nandini',
+        role: 'Developer',
+    }
+    const skills = ['React', 'TailWindCSS','Vite']
+    return(
+        <>
+        <h2>Passing Objects and Arrays as Props</h2>
+        <UserCard user={user}/>
+        <SkillsList skills={skills}/>
+        </>
+    )
+}
