e5f53ae3383823b0c1056455cbfc929cdce76341
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-05-06 13:07:43 +0530

    added useRef hook

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -5,12 +5,14 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import {UseContextIntro} from './components/p1.jsx'
 import {CustomHooksIntro} from './components/p2.jsx'
+import {UserRefIntro} from './components/p3.jsx'
 
 function App() {
   return(
     <>
         {/* <UseContextIntro/> */}
-        <CustomHooksIntro/>
+        {/* <CustomHooksIntro/> */}
+        <UserRefIntro/>
     </>
   )
 }
