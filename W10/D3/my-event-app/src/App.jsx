029d27c17becf2b30f5d00e41d9d16040326964d
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-05-07 09:57:25 +0530

    added Basic event concepts in react

---
+++ W10/D3/my-event-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import {EventBasics} from "./components/p1.jsx"
+
+function App() {
+  
+
+  return (
+    <>
+      <EventBasics/>
+    </>
+  )
+}
+
+export default App
