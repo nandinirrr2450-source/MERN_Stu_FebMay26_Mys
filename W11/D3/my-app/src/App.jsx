01c3adb0eaffbd71f76017c9d50d9dc97b361c65
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-05-13 14:54:20 +0530

    Managing API state with useEffect

---
+++ W11/D3/my-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import {ManagingApiState} from "./components/P1"
+import {UseEffectLifeCycle} from "./components/P2"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      {/* <ManagingApiState /> */}
+      <UseEffectLifeCycle/>
+    </>
+  )
+}
+
+export default App
