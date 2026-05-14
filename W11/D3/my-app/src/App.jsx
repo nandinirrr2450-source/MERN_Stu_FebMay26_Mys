5e3f732d2e635ae3ffd1c6b5147b173e3c5af062
nandinirrr2450-source <nandinirrr2450@gmail.com>
2026-05-14 12:20:56 +0530

    added  Error boundary & file upload progress

--- W11/D3/my-app/src/App.jsx
+++ W11/D3/my-app/src/App.jsx
@@ -7,6 +7,7 @@ import {UseEffectLifeCycle} from "./components/P2"
 import {AxiosLifecycle} from "./components/P3"
 import {CRUDOperation} from "./components/P4"
 import {PaginationCaching} from "./components/P5"
+import {UploadErrorBoundary} from "./components/P6"
 import './App.css'
 
 function App() {
@@ -18,7 +19,8 @@ function App() {
       {/* <UseEffectLifeCycle/> */}
       {/* <AxiosLifecycle/> */}
       {/* <CRUDOperation/> */}
-      <PaginationCaching/>
+      {/* <PaginationCaching/> */}
+      <UploadErrorBoundary/>
     </>
   )
 }
